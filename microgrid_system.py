4ed33cdab2d04e4c2c461217bdfe714757b61024
Sarah Newman <sarah.newman@pnnl.gov>
2025-08-19 10:51:14 -0700

    addl fix for resilience metrics cdf plots

--- microgrid_system.py
+++ microgrid_system.py
@@ -1786,7 +1786,7 @@ class SimpleMicrogridSystem(MicrogridSystem):
                                                                 metric_data[system_option]['cdf'][1],
                                                                 extrapolate=False)
                     new_y = interpolation_function(new_xaxis)
-                    metric_data[system_option]['cdf'] = (new_xaxis, new_y)
+                    metric_data[system_option]['cdf'] = (new_xaxis.tolist(), new_y.tolist())
 
         return res_metrics
 
